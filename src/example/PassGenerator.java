df6fce0c86832d3f942f012f44c79767cbd977bc
Keeeichiii <renassannn@gmail.com>
2024-10-17 16:16:00 +0300

    fixed structure and added test class

--- src/example/PassGenerator.java
+++ src/example/PassGenerator.java
@@ -1,21 +1,28 @@
 package example;
+
 import java.util.Random;
 
-class TestGenerator{
-    public static void main(String[]args){
-        PassGenerator meow=new PassGenerator();
-        meow.FinalGeneration();
-    }
-}
-class PassGenerator{
+
+
+class PasswordPartsGenerator {
+
     Random ran_num = new Random();
     StringBuilder chars = new StringBuilder();
     StringBuilder ints = new StringBuilder();
     StringBuilder specChars = new StringBuilder();
     StringBuilder finalPassword = new StringBuilder();
     String specialChar = "#$%^&*()!@[]{};:'\"<>,.?/\\|+-";
+    private String shuffpass;
 
-    public void CharGenerator() {
+    public String getShuffpass() {
+        return shuffpass;
+    }
+
+    public void setShuffpass(String shuffpass) {
+        this.shuffpass = shuffpass;
+    }
+
+    protected void CharGenerator() {
         for (int i = 0; i < 3; i++) {
             char rand_char = (char) (ran_num.nextInt(26) + 'a');
             chars.append(rand_char);
@@ -26,20 +33,24 @@ class PassGenerator{
         }
     }
 
-    public void IntegerGenerator(){
+    protected void IntegerGenerator() {
         for (int i = 0; i < 3; i++) {
             int rand_integer = ran_num.nextInt(10);
             ints.append(rand_integer);
         }
     }
-    public void SpecCharGenerator(){
+
+    protected void SpecCharGenerator() {
         for (int i = 0; i < 3; i++) {
             int index = ran_num.nextInt(specialChar.length());
             char speclChar = specialChar.charAt(index);
             specChars.append(speclChar);
         }
     }
-    public void FinalGeneration(){
+}
+
+public class PassGenerator extends PasswordPartsGenerator {
+    protected void FinalGeneration() {
         CharGenerator();
         IntegerGenerator();
         SpecCharGenerator();
@@ -55,7 +66,11 @@ class PassGenerator{
             passwordArray[j] = cash;
         }
         String shuffpass = new String(passwordArray);
-        System.out.println("Сгенерированный пароль: " + shuffpass);
+        setShuffpass(shuffpass);
     }
 
+    public void DisollayInfo() {
+        FinalGeneration();
+        System.out.println("Сгенерированный пароль: " + getShuffpass());
+    }
 }
---
+++ src/example/TestGenerator.java
@@ -0,0 +1,8 @@
+package example;
+
+class TestGenerator {
+    public static void main(String[] args) {
+        PassGenerator meow = new PassGenerator();
+        meow.DisollayInfo();
+    }
+}
