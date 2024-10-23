7570e091a6c5fee1188c484a3abbfa0da93cae98
Keeeichiii <renassannn@gmail.com>
2024-10-24 00:09:25 +0300

    test class inside the package

--- src/example/TestGenerator.java
+++ src/example/TestGenerator.java
@@ -1,8 +1,9 @@
-import  example.PassGenerator;
+package example;
+
 class TestGenerator {
     public static void main(String[] args) {
+        //для безопасноти можно вынести этот класс за пределы пакета:)
         PassGenerator call = new PassGenerator();
         call.DisplayInfo();
-
     }
 }
